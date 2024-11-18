ca2cf273daf4564f40042459414901bee55c43ca
Iqbal Hossain Sourov <princesourov41@gmail.com>
2024-11-18 12:29:35 +0600

    Android code of my application

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -179,4 +179,24 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+    override fun onRestart() {
+        super.onRestart()
+    }
+    override fun onPause() {
+        super.onPause()
+    }
+    override fun onStop() {
+        super.onStop()
+    }
 }
